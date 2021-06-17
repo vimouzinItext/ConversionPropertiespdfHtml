ecc28bcc1ac9bc92c9b7bc3a0097c2fe961ee1a9
vimouzinItext <68127958+vimouzinItext@users.noreply.github.com>
2021-06-17 11:50:06 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,12 +1,7 @@
-package ReusingHtml;
+package UsingPdfHtml;
 import com.itextpdf.html2pdf.ConverterProperties;
 import com.itextpdf.html2pdf.HtmlConverter;
-import com.itextpdf.html2pdf.attach.impl.DefaultTagWorkerFactory;
-import com.itextpdf.html2pdf.attach.impl.OutlineHandler;
-import com.itextpdf.html2pdf.css.apply.impl.DefaultCssApplierFactory;
-import com.itextpdf.html2pdf.resolver.font.DefaultFontProvider;
 import com.itextpdf.kernel.pdf.PdfWriter;
-import com.itextpdf.styledxmlparser.css.media.MediaDeviceDescription;
 import java.io.FileInputStream;
 import java.io.IOException;
 
@@ -14,19 +9,11 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         ConverterProperties properties = new ConverterProperties();
-        setConversionProperties(properties);
-        for (int i = 0; i < 20; i ++){
-            HtmlConverter.convertToPdf(new FileInputStream("inputHtml.html"),new PdfWriter("out"+i+".pdf"),properties);
-        }
+        setBaseUri(properties);
+        HtmlConverter.convertToPdf(new FileInputStream("inputHtml.html"),new PdfWriter("output.pdf"),properties);
     }
 
-    public static void setConversionProperties(ConverterProperties properties){
-        properties.setBaseUri(".")
-                .setCreateAcroForm(false)
-                .setCssApplierFactory(new DefaultCssApplierFactory())
-                .setFontProvider(new DefaultFontProvider())
-                .setMediaDeviceDescription(MediaDeviceDescription.createDefault())
-                .setOutlineHandler(new OutlineHandler())
-                .setTagWorkerFactory(new DefaultTagWorkerFactory());
+    public static void setBaseUri(ConverterProperties properties){
+        properties.setBaseUri("\\Example\\main\\Resources\\HtmlResources");
     }
 }
